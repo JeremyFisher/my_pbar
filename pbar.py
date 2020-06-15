ca7b90a8c7972f26ced2f84e02a82adaf9c3f756
JeremyFisher <78lhar@gmail.com>
2020-06-15 13:23:12 +1000

    Update pbar.py
    installs pbar in case we dont have it

--- pbar.py
+++ pbar.py
@@ -1,7 +1,19 @@
-from progressbar import AnimatedMarker, Bar, BouncingBar, Counter, ETA, \
+from os import system
+try:
+    from progressbar import AnimatedMarker, Bar, BouncingBar, Counter, ETA, \
         AdaptiveETA, FileTransferSpeed, FormatLabel, Percentage, \
         ProgressBar, ReverseBar, RotatingMarker, \
         SimpleProgress, Timer
+except:
+        cmd = 'pip install progressbar'
+        blah = input('Installing pbar...(press ctrl+c to quit)')
+        system(cmd)
+        from progressbar import AnimatedMarker, Bar, BouncingBar, Counter, ETA, \
+            AdaptiveETA, FileTransferSpeed, FormatLabel, Percentage, \
+            ProgressBar, ReverseBar, RotatingMarker, \
+            SimpleProgress, Timer        
+        
+       
 
 widgets = [Percentage(), 
         ' ', Bar(),
@@ -11,6 +23,9 @@ widgets = [Percentage(),
 def get_pbar():
     '''
     uses https://pypi.org/project/progressbar/#files
+    Usage:
+    for x in pbar(my_list):
+        ...
     '''
     pbar = ProgressBar(widgets=widgets)
     return pbar
